4b2234cf626dd8015e339a4bc881bf03f23c2814
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2024-08-15 01:06:30 +0700

    feat: added format date helper function

---
+++ client/src/utils/formatDate.ts
@@ -0,0 +1,4 @@
+export function formatDate(date: string) {
+    const d = new Date(date);
+    return `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+  }
