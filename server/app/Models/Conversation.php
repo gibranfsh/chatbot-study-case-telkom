a19440b2b5ffbc048bb5f3df958b62b9f7235b81
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2024-08-14 16:09:38 +0700

    chore: added Message and Conversation model and migration

---
+++ server/app/Models/Conversation.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Conversation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title'];
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->id = (string) Str::uuid();
+        });
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+}
---
+++ server/app/Models/Message.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['conversation_id', 'sender', 'content'];
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->id = (string) Str::uuid();
+        });
+    }
+
+    public function conversation()
+    {
+        return $this->belongsTo(Conversation::class);
+    }
+}
