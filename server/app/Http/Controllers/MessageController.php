047917bbb60247649d3a25e6e342cb1c29642dc9
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2024-08-14 23:49:18 +0700

    refactor: refactored message creation logic

--- server/app/Http/Controllers/MessageController.php
+++ server/app/Http/Controllers/MessageController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class MessageController extends Controller
 {
     public function store(Request $request)
     {
-        $message = Message::create($request->all());
+        $message = Message::create([
+            'conversation_id' => $request->conversation_id,
+            'sender' => $request->sender,
+            'content' => $request->content
+        ]);
+
         return response()->json($message, 201);
     }
 }
