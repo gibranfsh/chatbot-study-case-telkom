12586e58a47c07fa7d356a5c04c4039b7e275cd9
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2024-08-14 19:37:57 +0700

    feat: added API for storing conversations and messages

---
+++ server/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\ConversationController;
+use App\Http\Controllers\MessageController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/conversations', [ConversationController::class, 'index']);
+Route::post('/conversations', [ConversationController::class, 'store']);
+Route::get('/conversations/{id}', [ConversationController::class, 'show']);
+
+Route::post('/messages', [MessageController::class, 'store']);
