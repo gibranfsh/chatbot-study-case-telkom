b30ad48aaf7532b96020a9f4d5a7db523b99cea4
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2024-08-15 01:21:09 +0700

    refactor: grouped API routes with the same prefix

--- server/routes/api.php
+++ server/routes/api.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\ConversationController;
 use App\Http\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/conversations', [ConversationController::class, 'index']);
-Route::post('/conversations', [ConversationController::class, 'store']);
-Route::get('/conversations/{id}', [ConversationController::class, 'show']);
-Route::post('/conversations/{conversation_id}/messages', [MessageController::class, 'store']);
+Route::prefix('conversations')->group(function () {
+    Route::get('/', [ConversationController::class, 'index']);
+    Route::post('/', [ConversationController::class, 'store']);
+    Route::get('/{id}', [ConversationController::class, 'show']);
+    Route::post('/{conversation_id}/messages', [MessageController::class, 'store']);
+});
